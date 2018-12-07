51f21f642b49ee2c8f8c8f2bda91f74f8347fd45
Timwi <github@timwi.de>
2018-12-07 12:07:00 +0100

    More startup messages

--- Assets/SouvenirModule.cs
+++ Assets/SouvenirModule.cs
@@ -232,59 +232,63 @@ public class SouvenirModule : MonoBehaviour
 
         disappear();
         SetWordWrappedText(Ut.NewArray(
-            "I see dead defusers.",
-            "Welcome... to the real bomb.",
-            "I’m gonna make him a bomb he can’t defuse.",
-            "Defuse it again, Sam.",
-            "Louis, I think this is the beginning of a beautiful explosion.",
-            "Here’s looking at you, defuser.",
-            "Hey. I could defuse this bomb in ten seconds flat.",
-            "Go ahead, solve my bomb.",
-            "May the bomb be with you.",
-            "I love the smell of explosions in the morning.",
-            "Blowing up means never having to say you’re sorry.",
-            "The stuff that bombs are made of.",
-            "E.T. defuse bomb.",
-            "Bomb. James Bomb.",
-            "You can’t handle the bomb!",
-            "Blow up the usual suspects.",
-            "You’re gonna need a bigger bomb.",
-            "Bombs are like a box of chocolates. You never know what you’re gonna get.",
-            "Houston, we have a module.",
-            "Elementary, my dear expert.",
-            "Forget it, Jake, it’s KTANE.",
-            "I have always depended on the fitness of experts.",
-            "A bomb. Exploded, not defused.",
-            "I’m the king of the bomb!",
-            "Blow me up, Scotty.",
-            "Yabba dabba boom!",
-            "This bomb will self-destruct in five seconds.",
-            "Defusing is futile.",
-            "Is that your final answer?",
-            "A bomb’s best friend is his defuser.",
-            "Keep your experts close, but your bomb closer.",
-            "Fasten your seatbelts. It’s going to be a bomby night.",
-            "Show me the modules!",
-            "We defuse bombs.",
-            "We’ll always have batteries.",
-            "Say hello to my little bomb.",
-            "You’re a defuser, Harry.",
-            "I’m sorry, Dave. I’m afraid I can’t defuse that.",
-            "You either die a defuser, or you live long enough to see yourself become the expert.",
-            "This isn’t defusing. This is exploding... with style.",
-            "Could you describe the module, sir?",
-            "You want widgets? I got twenty.",
-            "We don’t need no stinking widgets.",
-            "Say edgework one more goddamn time.",
-            "How do you like them modules?",
-            "Introducing: The Double... Decker... Bomb!",
-            "Have you got your wires crossed?",
-            "Don't cross the wires.",
-            "Wanna hear the most annoying explosion in the world?",
-            "Manuals? Where we’re going, we don’t need manuals.",
-            "On a long enough timeline, the survival rate for everyone drops to zero.",
-            "This is your bomb, and it's ending one minute at a time.",
-            "The first rule of defusal is, you keep talking about defusal."
+            "I see dead defusers.",     // “I see dead people”, (Sixth Sense)
+            "Welcome... to the real bomb.",     // “Welcome... to the real world.” (The Matrix)
+            "I’m gonna make him a bomb he can’t defuse.",   // “I’m gonna make him an offer he can’t refuse.” (The Godfather)
+            "Defuse it again, Sam.",    // “Play it again, Sam.” (Casablanca) (misquote)
+            "Louis, I think this is the beginning of a beautiful explosion.",   // “Louis, I think this is the beginning of a beautiful friendship.” (Casablanca)
+            "Here’s looking at you, defuser.",  // “Here’s looking at you, kid.” (Casablanca)
+            "Hey. I could defuse this bomb in ten seconds flat.",   // “Hey. I could clear the sky in ten seconds flat.” (MLP:FiM, Friendship is Magic - Part 1)
+            "Go ahead, solve my bomb.", // “Go ahead, make my day.” (Sudden Impact / Dirty Harry series)
+            "May the bomb be with you.",    // “May the Force be with you.” (Star Wars IV: A New Hope)
+            "I love the smell of explosions in the morning.",   // “I love the smell of napalm in the morning.” (Apocalypse Now)
+            "Blowing up means never having to say you’re sorry.",   // “Love means never having to say you're sorry.” (Love Story)
+            "The stuff that bombs are made of.",    // “The Stuff That Dreams Are Made Of” (“Coming Around Again” album by Carly Simon)
+            "E.T. defuse bomb.",    // “E.T. phone home.” (E.T. the Extra-Terrestrial)
+            "Bomb. James Bomb.",    // “Bond. James Bond.” (Dr. No / James Bond series)
+            "You can’t handle the bomb!",   // “You can’t handle the truth!” (A Few Good Men)
+            "Blow up the usual suspects.",  // “Round up the usual suspects.” (Casablanca)
+            "You’re gonna need a bigger bomb.", // “You’re gonna need a bigger boat.” (Jaws)
+            "Bombs are like a box of chocolates. You never know what you’re gonna get.",    // “My mom always said life was like a box of chocolates. You never know what you're gonna get.” (Forrest Gump)
+            "Houston, we have a module.",   // “Houston, we have a problem.” (Apollo 13)
+            "Elementary, my dear expert.",  // “Elementary, my dear Watson.” (Sherlock Holmes) (misquote)
+            "Forget it, Jake, it’s KTANE.",     // “Forget it, Jake, it’s Chinatown.” (Chinatown)
+            "I have always depended on the fitness of experts.",    // “I’ve always depended on the kindness of strangers.” (A Streetcar Named Desire)
+            "A bomb. Exploded, not defused.",   // “A Martini. Shaken, not stirred.” (Diamonds Are Forever (novel) / James Bond)
+            "I’m the king of the bomb!",    // “I’m the king of the world!” (Titanic)
+            "Blow me up, Scotty.",  // “Beam me up, Scotty!” (Star Trek misquote)
+            "Yabba dabba boom!",    // “Yabba dabba doo!” (Flintstones)
+            "This bomb will self-destruct in five seconds.",    // “This tape will self-destruct in five seconds.” (Mission: Impossible)
+            "Defusing is futile.",  // “Resistance is futile.” (Star Trek: The Next Generation)
+            "Is that your final answer?",   // direct quote (Who Wants to be a Millionaire?)
+            "A bomb’s best friend is his defuser.", // “A man’s best friend is his dog.” (attorney George Graham Vest, 1870 Warrensburg)
+            "Keep your experts close, but your bomb closer.",   // “Keep your friends close and your enemies closer.” (The Prince / Machiavelli)
+            "Fasten your seatbelts. It’s going to be a bomby night.",   // “Fasten your seat belts, it’s going to be a bumpy night.” (All About Eve)
+            "Show me the modules!", // “Show me the money!” (Jerry Maguire)
+            "We’ll always have batteries.", // “We’ll always have Paris.” (Casablanca)
+            "Say hello to my little bomb.", // “Say hello to my little friend!” (Scarface)
+            "You’re a defuser, Harry.", // “You’re a wizard, Harry.” (Harry Potter and the Philosopher’s Stone)
+            "I’m sorry, Dave. I’m afraid I can’t defuse that.", // “I’m sorry, Dave. I’m afraid I can’t do that.” (2001: A Space Odyssey)
+            "You either die a defuser, or you live long enough to see yourself become the expert.", // “Well, I guess you either die a hero or you live long enough to see yourself become the villain.” (The Dark Knight)
+            "This isn’t defusing. This is exploding... with style.",    // “This isn’t flying. This is falling... with style.” (Toy Story)
+            "Could you describe the module, sir?",  // “Could you describe the ruckus, sir?” (The Breakfast Club)
+            "You want widgets? I got twenty.",  // “You want thingamabobs? I got twenty.” (The Little Mermaid)
+            "We don’t need no stinking widgets.",   // “We don’t need no stinking badges!” (The Treasure of the Sierra Madre)
+            "Say edgework one more goddamn time.",  // “Say what one more goddamn time.” (Pulp Fiction)
+            "How do you like them modules?",    // “How do you like them apples?” (Good Will Hunting)
+            "Introducing: The Double... Decker... Bomb!",   // “Introducing: The Double... Decker... Couch!” (The LEGO Movie)
+            "Have you got your wires crossed?", // “Have you got your lions crossed?” (The Lion King)
+            "Don't cross the wires.",   // “Don’t cross the streams.” (Ghostbusters)
+            "Wanna hear the most annoying explosion in the world?", // “Wanna hear the most annoying sound in the world?” (Dumb & Dumber)
+            "Manuals? Where we’re going, we don’t need manuals.",   // “Roads? Where we’re going, we don’t need roads.” (Back to the Future)
+            "On a long enough time line, the survival rate for everyone will drop to zero.", // direct quote (Fight Club (novel))
+            "This is your bomb, and it's ending one minute at a time.", // “This is your life and it’s ending one minute at a time.” (Fight Club)
+            "The first rule of defusal is, you keep talking about defusal.",    // “The first rule of Fight Club is, you don’t talk about Fight Club.” (Fight Club)
+            "Well, here's another nice mess you’ve gotten me into!",     // direct quote (Sons of the Desert / Oliver Hardy)
+            "You know how to defuse, don’t you, Steve? You just put your wires together and cut.",  // “You know how to whistle, don't you Steve? You just put your lips together, and blow.” (To Have And Have Not)
+            "Mrs. Defuser, you’re trying to disarm me. Aren’t you?",    // “Mrs. Robinson, you’re trying to seduce me. Aren’t you?” (The Graduate)
+            "We defuse bombs.",  // “We rob banks.” (Bonnie and Clyde)
+            "Somebody set up us the bomb."  // direct quote (Zero Wing)
         ).PickRandom(), 1.75);
 
         if (transform.parent != null)
