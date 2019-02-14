f5c0435cc58476cc14cd129c9bbb59f96dc65271
caitsith2 <d_good@caitsith2.com>
2019-02-14 12:49:29 -0800

    Turtle Robot now uses its GlassTTY Font for answers.

--- Assets/Question.cs
+++ Assets/Question.cs
@@ -443,8 +443,8 @@ namespace Souvenir
             ExampleExtraFormatArguments = new[] { "departure", "destination" }, ExampleExtraFormatArgumentGroupSize = 1)]
         TimezoneCities,
 
-		[SouvenirQuestion("What was the {1} line you commented out in {0}?", "Turtle Robot", 6, ExampleAnswers = new [] {"lt 90", "fd 1", "rt 180 2", "lt 90 2", "rt 180", "fd 6", "rt 90 2"},
-			ExampleExtraFormatArguments = new[] {"first", "second"}, ExampleExtraFormatArgumentGroupSize = 2)]
+		[SouvenirQuestion("What was the {1} line you commented out in {0}?", "Turtle Robot", 4, ExampleAnswers = new [] {"lt 90", "fd 1", "rt 180 2", "lt 90 2", "rt 180", "fd 6", "rt 90 2"},
+			ExampleExtraFormatArguments = new[] {"first", "second"}, ExampleExtraFormatArgumentGroupSize = 1, Type = AnswerType.GlassTTYFont)]
 		TurtleRobotCodeLines,
 
         [SouvenirQuestion("What was the {1} correct query response from {0}?", "Two Bits", 6, "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
@@ -475,6 +475,7 @@ namespace Souvenir
     {
         Default,
         SymbolsFont,
+		GlassTTYFont,
         Sprites
     }
 
