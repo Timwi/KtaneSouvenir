e4bc76a2e96ed8aefa6cf7cbaf64f38375ef84a6
Timwi <github@timwi.de>
2024-11-19 15:22:06 +0100

    LED Grid fix: make sure that multiple Souvenirs will be able to see the LEDs correctly

--- Lib/ModulesL.cs
+++ Lib/ModulesL.cs
@@ -173,13 +173,15 @@ public partial class SouvenirModule
     private IEnumerator<YieldInstruction> ProcessLEDGrid(ModuleData module)
     {
         var comp = GetComponent(module, "ledGridScript");
-        yield return WaitForSolve;
+        yield return null;
 
         var lights = GetListField<Renderer>(comp, "lights", isPublic: true).Get(expectedLength: 9);
         var colors = GetListField<Texture>(comp, "colours", isPublic: true).Get(expectedLength: 11);
         var blackColor = colors.FirstOrDefault(c => c.name == "black")
             ?? throw new AbandonModuleException("Abandoning LED Grid because ‘colours’ does not contain a texture named ‘black’.");
         var numBlack = lights.Count(l => l.material.mainTexture.name == "black");
+
+        yield return WaitForSolve;
 
         // Change all of the LEDs to black
         foreach (var light in lights)
