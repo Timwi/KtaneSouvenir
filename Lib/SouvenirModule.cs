2953ab047bc07cc75911a1f0aa52ae45ba384540
Hawker <kovujacksonbentley@gmail.com>
2024-12-03 12:43:33 -0500

    Fix asking questions for autosolved modules

--- Lib/SouvenirModule.cs
+++ Lib/SouvenirModule.cs
@@ -757,7 +757,7 @@ public partial class SouvenirModule : MonoBehaviour
                     else
                         yield return e.Current;
 
-                    if (false)
+                    if (TwitchAbandonModule.Contains(module))
                     {
                         Debug.Log($"<Souvenir #{_moduleId}> Abandoning {module.ModuleDisplayName} because Twitch Plays told me to.");
                         _coroutinesActive--;
