562b104aa1c674bd78c51f1d95f5ae9276703563
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-03-02 18:50:22 -0500

    Update docstrings

--- Lib/ModuleData.cs
+++ Lib/ModuleData.cs
@@ -4,11 +4,11 @@
     {
         /// <summary>The actual module component</summary>
         public KMBombModule Module;
-        /// <summary>Set to <code>true</code> after <code>KMBombModule.HandlePass()</code> has been called.</summary>
+        /// <summary>Set to <see langword="true"/> after <see cref="KMBombModule.HandlePass"/> has been called.</summary>
         public bool IsSolved => !Unsolved;
-        /// <summary>Set to <code>false</code> after <code>KMBombModule.HandlePass()</code> has been called.</summary>
+        /// <summary>Set to <see langword="false"/> after <see cref="KMBombModule.HandlePass"/> has been called.</summary>
         public bool Unsolved = true;
-        /// <summary>The order in which this module has been solved, or 0 if it is currently unsolved.</summary>
+        /// <summary>The order in which this module has been solved, or <see langword="0"/> if it is currently unsolved.</summary>
         public int SolveIndex;
     }
 }
