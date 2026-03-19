d8103bc5d80f1241ba9a8c811f8828dfddd21e66
Espik <sbick23@outlook.com>
2026-03-19 18:58:44 -0500

    Dr. Doctor update

--- Lib/Handlers/DrDoctor.cs
+++ Lib/Handlers/DrDoctor.cs
@@ -7,10 +7,10 @@ using static Souvenir.AnswerLayout;
 
 public enum SDrDoctor
 {
-    [SouvenirQuestion("Which of these diseases was listed on {0}, but not the one treated?", TwoColumns4Answers, "Alztimer’s", "Braintenance", "Color allergy", "Detonession", "Emojilepsy", "Foot and Morse", "Gout of Life", "HRV", "Indicitis", "Jaundry", "Keypad stones", "Legomania", "Microcontusion", "Narcolization", "OCd", "Piekinson’s", "Quackgrounds", "Royal Flu", "Seizure Siphor", "Tetrinus", "Urinary LEDs", "Verticode", "Widgeting", "XMAs", "Yes-no infection", "Zooties", "Chronic Talk", "Jukepox", "Neurolysis", "Perspective Loss", "Orientitis", "Huntington’s disease")]
+    [SouvenirQuestion("Which of these diseases was listed on {0}, but not the one treated?", OneColumn4Answers, "Alztimer’s", "Braintenance", "Color allergy", "Detonession", "Emojilepsy", "Foot and Morse", "Gout of Life", "HRV", "Indicitis", "Jaundry", "Keypad stones", "Legomania", "Microcontusion", "Narcolization", "OCd", "Piekinson’s", "Quackgrounds", "Royal Flu", "Seizure Siphor", "Tetrinus", "Urinary LEDs", "Verticode", "Widgeting", "XMAs", "Yes-no infection", "Zooties", "Chronic Talk", "Jukepox", "Neurolysis", "Perspective Loss", "Orientitis", "Huntington’s disease")]
     Diseases,
 
-    [SouvenirQuestion("Which of these symptoms was listed on {0}?", TwoColumns4Answers, "Bloating", "Chills", "Cold Hands", "Constipation", "Cough", "Diarrhea", "Disappearance of the Ears", "Dizziness", "Excessive Crying", "Fatigue", "Fever", "Foot swelling", "Gas", "Hallucination", "Headache", "Loss of Smell", "Muscle Cramp", "Nausea", "Numbness", "Shortness of Breath", "Sleepiness", "Thirstiness", "Throat irritation")]
+    [SouvenirQuestion("Which of these symptoms was listed on {0}?", OneColumn4Answers, "Bloating", "Chills", "Cold Hands", "Constipation", "Cough", "Diarrhea", "Disappearance of the Ears", "Dizziness", "Excessive Crying", "Fatigue", "Fever", "Foot swelling", "Gas", "Hallucination", "Headache", "Loss of Smell", "Muscle Cramp", "Nausea", "Numbness", "Shortness of Breath", "Sleepiness", "Thirstiness", "Throat irritation")]
     Symptoms
 }
 
@@ -26,8 +26,9 @@ public partial class SouvenirModule
         var diagnoses = GetArrayField<string>(comp, "_selectableDiagnoses").Get();
         var symptoms = GetArrayField<string>(comp, "_selectableSymptoms").Get();
         var diagnoseText = GetField<TextMesh>(comp, "DiagnoseText", isPublic: true).Get();
+        var syptomText = GetField<TextMesh>(comp, "SympText", isPublic: true).Get();
 
         yield return question(SDrDoctor.Diseases).Answers(diagnoses.Except([diagnoseText.text]).ToArray());
-        yield return question(SDrDoctor.Symptoms).Answers(symptoms);
+        yield return question(SDrDoctor.Symptoms).Answers(symptoms.Except([syptomText.text]).ToArray(), all: SDrDoctor.Symptoms.GetAnswers().Except([syptomText.text]).ToArray());
     }
 }
