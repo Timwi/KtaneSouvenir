b09770a0cfd04116217d13e42b742c97259cc816
QuinnWuest <qwuest123@gmail.com>
2026-04-08 11:09:34 -0400

    Fix off-by-1 font indexing

--- Lib/AnswerType.cs
+++ Lib/AnswerType.cs
@@ -5,19 +5,18 @@ public enum AnswerType
     // These values must match the indexes in SouvenirModule.Fonts/SouvenirModule.FontTextures
     Default = 0,
     SymbolsFont = 1,    // used by 3D Tunnels, Shape Shift, Switches and Colored Switches
-    TurtleRobotFont = 2,
-    TicTacToeFont = 3,
-    UnownFont = 4,
-    SugarSkullsFont = 5,
-    AsciiMazeFont = 6, // also used by Forget This
-    PianoKeysFont = 7,
-    JapaneseFont = 8,   // for the Japanese translation
-    SnowflakesFont = 9,
-    SixtyFourFont = 10,
-    CrypticCycleBoozleglyphFont = 11,
-    AMessageFont = 12,
-    FourDTunnelsFont = 13,
-    CubeFont = 14,
+    TicTacToeFont = 2,
+    UnownFont = 3,
+    SugarSkullsFont = 4,
+    AsciiMazeFont = 5, // also used by Forget This
+    PianoKeysFont = 6,
+    JapaneseFont = 7,   // for the Japanese translation
+    SnowflakesFont = 8,
+    SixtyFourFont = 9,
+    CrypticCycleBoozleglyphFont = 10,
+    AMessageFont = 11,
+    FourDTunnelsFont = 12,
+    CubeFont = 13,
 
     DynamicFont = -1, // Special value to indicate that the module handler will obtain the font from the client module
     Sprites = -2,     // Special value for answers that use sprites
