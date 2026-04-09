716b89871f82b48f8d61c3a98599c67d7eb9df1b
Espik <sbick23@outlook.com>
2026-04-08 22:20:22 -0500

    Additional font fixes

--- Lib/AnswerType.cs
+++ Lib/AnswerType.cs
@@ -6,17 +6,16 @@ public enum AnswerType
     Default = 0,
     SymbolsFont = 1,    // used by 3D Tunnels, Shape Shift, Switches and Colored Switches
     TicTacToeFont = 2,
-    UnownFont = 3,
-    SugarSkullsFont = 4,
-    AsciiMazeFont = 5, // also used by Forget This
-    PianoKeysFont = 6,
-    JapaneseFont = 7,   // for the Japanese translation
-    SnowflakesFont = 8,
-    SixtyFourFont = 9,
-    CrypticCycleBoozleglyphFont = 10,
-    AMessageFont = 11,
-    FourDTunnelsFont = 12,
-    CubeFont = 13,
+    SugarSkullsFont = 3,
+    AsciiMazeFont = 4, // also used by Forget This
+    PianoKeysFont = 5,
+    JapaneseFont = 6,   // for the Japanese translation
+    SnowflakesFont = 7,
+    SixtyFourFont = 8,
+    CrypticCycleBoozleglyphFont = 9,
+    AMessageFont = 10,
+    FourDTunnelsFont = 11,
+    CubeFont = 12,
 
     DynamicFont = -1, // Special value to indicate that the module handler will obtain the font from the client module
     Sprites = -2,     // Special value for answers that use sprites
